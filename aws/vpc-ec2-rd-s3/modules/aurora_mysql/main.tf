924374422f5e9998fe5f1bbf84d43fc3e37973bb
nasirnjs <nasirnjs@gmail.com>
2024-11-25 21:01:39 +0600

    add rds server less custer

--- aws/vpc-ec2-rd-s3/modules/aurora_mysql/main.tf
+++ aws/vpc-ec2-rd-s3/modules/aurora_mysql/main.tf
@@ -21,6 +21,7 @@ resource "aws_rds_cluster" "rds_cluster" {
   master_username    = "test"
   master_password    = "Pass4fsfsfYour34FDS"
   deletion_protection = false
+  skip_final_snapshot = true
 
   # Associate the DB Subnet Group with the RDS Cluster
   db_subnet_group_name = aws_db_subnet_group.ym_rds_subnet_group.name
