f96960cd217d8e82715754cbc49cf3bf6a810f35
nasirnjs <nasirnjs@gmail.com>
2025-04-26 00:33:15 +0600

    remove tf basic

--- terraform_associate_003/01_terrafrom_basic/01_init_plan_apply/main.tf
+++
@@ -1,5 +0,0 @@
-resource "local_sensitive_file" "foo" {
-  content  = "foo!"
-  filename = "${path.module}/foo.bar"
-  file_permission = "775"
-}
--- terraform_associate_003/01_terrafrom_basic/02_variable/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-output "available_zones" {
-  description = "List of availability zones"
-  value       = var.availability_zones
-}
