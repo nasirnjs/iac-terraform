d9356dbd63d22713ee6c42440804f07bd0fd5294
nasirnjs <nasirnjs@gmail.com>
2026-05-02 10:21:08 +0600

    Remove legacy EKS private cluster Terraform configuration

--- aws/eks-private-cluster/modules/eks/output.tf
+++
@@ -1,6 +0,0 @@
-
-
-# output "identity-oidc-issuer" {
-#   # value = data.aws_eks_cluster.example.identity[0].oidc[0].issuer
-#   value = aws_eks_cluster.cluster_quickops.identity[0].oidc[0].issuer
-# }
